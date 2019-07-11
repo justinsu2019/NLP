14d9dcd94dc5127a240ee59043526938e4a9ceb2
justinsu2019 <47746000+justinsu2019@users.noreply.github.com>
2019-07-12 05:16:09 +0800

    Update phrase pickout.py

--- phrase pickout.py
+++ phrase pickout.py
@@ -2,6 +2,8 @@
 to pick phrase, there should be 2 steps
 1) pick out the phrase regardless what the phrase means -- there is barely anything to do with NLP in this step. or not so much NLP.
    a) the only NLP we use here is clean up the text and make it ready in use.
-   b) after cleaning up we can 
+   b) after cleaning up we need to pickout the words appears the most in all inputs.
+   c) take out any type of words?
+   d) words order do matters.
 2) interpret the phrase out based on the actual acticle. -- 
 '''

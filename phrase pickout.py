f35c7bd7420a2a1c380f91e83fac47a50a3dda83
justinsu2019 <47746000+justinsu2019@users.noreply.github.com>
2019-07-12 04:49:23 +0800

    Create phrase pickout.py

---
+++ phrase pickout.py
@@ -0,0 +1,7 @@
+'''
+to pick phrase, there should be 2 steps
+1) pick out the phrase regardless what the phrase means -- there is barely anything to do with NLP in this step. or not so much NLP.
+   a) the only NLP we use here is clean up the text and make it ready in use.
+   b) after cleaning up we can 
+2) interpret the phrase out based on the actual acticle. -- 
+'''
